ea2e9a7aaa1a01e369bc751c1de79e6d85c97861
z <568356727@qq.com>
2020-03-13 15:16:59 +0800

    “first commit

---
+++ src/components/tourismCircle/PublishButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="page-head-button">
+    <img class="page-head-button-bg" src="/static/z/images/page-head-button.png" alt="">
+    <span class="page-head-button-content">发布</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "PublishButton"
+  }
+</script>
+
+<style scoped>
+
+  .page-head-button-bg{
+    width: 100%;
+    height: 100%;
+    border:0;
+  }
+  .page-head-button-content{
+    color: #FFF;
+    position: relative;
+    bottom: 70upx;
+    font-size:32upx;
+    font-family:Source Han Sans CN;
+    font-weight:bold;
+  }
+</style>
